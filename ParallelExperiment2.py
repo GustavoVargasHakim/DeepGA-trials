db1d1869b46abd03520d781f3c64dfedbbb570d4
Gustavo Vargas Hakim <zS19019837@estudiantes.uv.mx>
2020-12-11 19:49:14 -0600

    New parallel versions

--- ParallelExperiment2.py
+++ ParallelExperiment2.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 10 10:31:09 2020
-
-@author: user
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 20 18:37:31 2020
+Created on Wed Nov 25 12:15:38 2020
 
 @author: user
 """
@@ -34,56 +27,9 @@ from torch import nn
 from multiprocessing import Process, Manager
 import pickle
 
-def dominates(p, q):
-    if p[1] >= q[1] and p[2] <= q[2]:
-        if p[1] > q[1] or p[2] < q[2]:
-            dom = True
-        else:
-            dom = False
-    else:
-        dom = False
-            
-    return dom
-
-
-def dominance_sorting(Pop):
-    R = Pop[:]
-    F = []
-    while len(R) != 0:
-        p = R[0]
-        Fi = [p]
-        for q in [Q for Q in R if Q not in Fi]:
-            if len(Fi) == 1:
-                p = Fi[0]
-                if dominates(q, p):
-                    p = q
-                    Fi[0] = q
-                elif not dominates(p, q) and not dominates(q,p):
-                    Fi.append(q)
-            else:
-                n = 0
-                Fic = Fi[:]
-                for j in range(len(Fic)):
-                    f = Fic[j]
-                    if dominates(q, f):
-                        if q not in Fi:
-                            Fi[j] = q
-                        else:
-                            Fi.remove(f)
-                    elif not dominates(f, q) and not dominates(q,f):
-                        n += 1
-                if n == len(Fi):
-                    Fi.append(q)
-        F += Fi
-        for f in Fi:
-            R.remove(f)
-    
-    return F
-                
-
 #Random seed
-#random.seed(1)
-#torch.manual_seed(1)
+random.seed(9)
+torch.manual_seed(9)
 
 #Loading data
 train_dl, test_dl = loading_data()
@@ -107,7 +53,7 @@ max_full = 4
 cr = 0.7 #Crossover rate
 mr = 0.5 #Mutation rate
 N = 20 #Population size
-T = 20 #Number of generations
+T = 50 #Number of generations
 t_size = 5 #tournament size
 w = 0.3 #penalization weight
 max_params = 2e6
@@ -153,11 +99,11 @@ while len(pop) < N:
     training2.join()
     
     if acc_list[0][0] == '1':
-        pop.append([e1, acc_list[0][2], acc_list[0][3]])
-        pop.append([e2, acc_list[1][2], acc_list[1][3]])
+        pop.append([e1, acc_list[0][1], acc_list[0][2], acc_list[0][3]])
+        pop.append([e2, acc_list[1][1], acc_list[1][2], acc_list[1][3]])
     else:
-        pop.append([e2, acc_list[0][2], acc_list[0][3]])
-        pop.append([e1, acc_list[1][2], acc_list[1][3]])
+        pop.append([e2, acc_list[0][1], acc_list[0][2], acc_list[0][3]])
+        pop.append([e1, acc_list[1][1], acc_list[1][2], acc_list[1][3]])
 
 '''Genetic Algorithm'''
 for t in range(T):
@@ -219,39 +165,36 @@ for t in range(T):
             training2.join()
             
             if acc_list[0][0] == '1':
-                offspring.append([c1, acc_list[0][2], acc_list[0][3]])
-                offspring.append([c2, acc_list[1][2], acc_list[1][3]])
+                offspring.append([c1, acc_list[0][1], acc_list[0][2], acc_list[0][3]])
+                offspring.append([c2, acc_list[1][1], acc_list[1][2], acc_list[1][3]])
             else:
-                offspring.append([c2, acc_list[0][2], acc_list[0][3]])
-                offspring.append([c1, acc_list[1][2], acc_list[1][3]])
+                offspring.append([c2, acc_list[0][1], acc_list[0][2], acc_list[0][3]])
+                offspring.append([c1, acc_list[1][1], acc_list[1][2], acc_list[1][3]])
        
     #Replacement with elitism
     pop = pop + offspring
-    pop = dominance_sorting(pop)
+    pop.sort(reverse = True, key = lambda x: x[1])
     pop = pop[:N]
     
     leader = max(pop, key = lambda x: x[1])
-    #bestAcc.append(leader[2])
-    #bestF.append(leader[1])
-    #bestParams.append(leader[3])
+    bestAcc.append(leader[2])
+    bestF.append(leader[1])
+    bestParams.append(leader[3])
     
         
-    print('Best accuracy: ', leader[1])
-    print('Best No. of Params: ', leader[2])
+    print('Best fitness: ', leader[1])
+    print('Best accuracy: ', leader[2])
+    print('Best No. of Params: ', leader[3])
     print('No. of Conv. Layers: ', leader[0].n_block)
     print('No. of FC Layers: ', leader[0].n_full)
     print('--------------------------------------------')
 
+results = pd.DataFrame(list(zip(bestAcc, bestF, bestParams)), columns = ['Accuracy', 'Fitness', 'No. Params'])
+results.to_csv('/home/proy_ext_adolfo.vargas/DeepGA/results4.csv', index = False)
+results.to_csv('results4.csv', index = False)
 stop = timeit.default_timer()
 execution_time = (stop-start)/3600
 print("Execution time: ", execution_time)
-accuracy = []
-parameters = []
-for p in pop:
-    accuracy.append(p[1])
-    parameters.append(p[2])
-
-results = pd.DataFrame(list(zip(accuracy, parameters)), columns = ['Accuracy', 'No. Params'])
 final_networks = []
 final_connections = []
 objects = []
@@ -290,23 +233,21 @@ for member in pop:
         description += layer
     description += ' neurons'
     final_networks.append(description)
-
-     
+    
+        
 final_population = pd.DataFrame(list(zip(final_networks, final_connections)), columns = ['Network Architecture', 'Connections'])
 
 '''Saving Results as CSV'''
-final_population.to_csv('/home/proy_ext_adolfo.vargas/DeepGA/final_population3.csv', index = False)
-final_population.to_csv('final_population3.csv', index = False)
-results.to_csv('/home/proy_ext_adolfo.vargas/DeepGA/results3.csv', index = False)
-results.to_csv('results3.csv', index = False)      
+final_population.to_csv('/home/proy_ext_adolfo.vargas/DeepGA/final_population4.csv', index = False)
+final_population.to_csv('final_population4.csv', index = False)    
 
 
 #Saving objects
-with open('/home/proy_ext_adolfo.vargas/cnns3.pkl', 'wb') as output:
+with open('/home/proy_ext_adolfo.vargas/cnns4.pkl', 'wb') as output:
     pickle.dump(objects, output, pickle.HIGHEST_PROTOCOL)
     output.close()
 
-with open('cnns3.pkl', 'wb') as output:
+with open('cnns4.pkl', 'wb') as output:
     pickle.dump(objects, output, pickle.HIGHEST_PROTOCOL)
     output.close()
     
